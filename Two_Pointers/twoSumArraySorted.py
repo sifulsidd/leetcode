67b5ac462ed8d8ec7afe64efe9606a354958562f
sifulsidd <sifulsiddiki@yahoo.com>
2024-10-09 14:34:39 -0400

    first two pointer

---
+++ Two_Pointers/twoSumArraySorted.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            total = numbers[l] + numbers[r]
+            if total == target:
+                return [l+1,r+1]
+            elif total > target:
+                r-=1
+            else:
+                l += 1
+        return True
+        
