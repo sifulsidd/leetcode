01a5926f1cc4245ab5b8cfea0dfaea4e67ab043c
sifulsidd <sifulsiddiki@yahoo.com>
2024-10-09 00:54:33 -0400

    better solution

--- Arrays/encodeDecode.py
+++ Arrays/encodeDecode.py
@@ -22,3 +22,35 @@ class Codec:
         arr = s.split("1234567890")
         arr.pop()
         return arr
+    
+
+class Codec:
+
+    def encode(self, strs):
+        """Encodes a list of strings to a single string.
+        
+        :type strs: List[str]
+        :rtype: str
+        """
+        s = ""
+        for word in strs:
+            s += str(len(word)) +'#'+ word  
+        
+        return s
+
+
+    def decode(self, s):
+        """Decodes a single string to a list of strings.
+        
+        :type s: str
+        :rtype: List[str]
+        """
+        res, i = [], 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j+=1
+            length = int(s[i:j])
+            res.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return res 
