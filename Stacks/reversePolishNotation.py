8e97ec14f1fb7370962d67f400700692a5d507b3
sifulsidd <sifulsiddiki@yahoo.com>
2024-10-19 22:59:18 -0400

    Idea is not too difficult just remember the float() into the int() cast to round negative numbers into 0.

---
+++ Stacks/reversePolishNotation.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        
+        for token in tokens:
+            if token == "+":
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(val2 + val1)
+            elif token == "-":
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(val2 - val1)
+            elif token == "*":
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(val2*val1)
+            elif token == "/":
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(int(float(val2)/val1))
+            else:
+                stack.append(int(token))
+        return stack[0]
