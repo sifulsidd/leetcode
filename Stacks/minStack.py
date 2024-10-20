63919c8eb8a52b3df3d242f0a3027f3242669ecf
sifulsidd <sifulsiddiki@yahoo.com>
2024-10-19 22:45:03 -0400

    minStack using 2 stacks instead of one

--- Stacks/minStack.py
+++ Stacks/minStack.py
@@ -35,3 +35,43 @@ class MinStack(object):
         :rtype: int
         """
         return self.arr[-1][1]
+
+# using 2 stacks instead of 1
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        if len(self.min) == 0:
+            self.min.append(val)
+        else:
+            currMin = self.min[-1]
+            newMin = min(currMin, val)
+            self.min.append(newMin)
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min[-1]
