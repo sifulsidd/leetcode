9772235cc84a5f2f1c967663c62097f913c552bb
sifulsidd <sifulsiddiki@yahoo.com>
2024-10-20 01:12:47 -0400

    Don't need stacks

--- Stacks/carFleet.py
+++ Stacks/carFleet.py
@@ -19,3 +19,24 @@ class Solution(object):
                 stack.append(time)
             
         return len(stack)
+    
+    
+# non stacks
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        combined = [[position[i], speed[i]] for i in range(len(position))]
+        
+        total = 0
+        curTime = 0
+        for p,s in sorted(combined, reverse = True):
+            time = (target - p)/float(s)
+            if time > curTime:
+                total += 1
+                curTime = time
+        return total
