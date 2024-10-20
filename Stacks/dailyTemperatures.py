edc7bd8074038e66d1318b5985d0ed63f25ade09
sifulsidd <sifulsiddiki@yahoo.com>
2024-10-20 00:19:34 -0400

    Add the while loop and bang

--- Stacks/dailyTemperatures.py
+++ Stacks/dailyTemperatures.py
@@ -12,3 +12,20 @@ class Solution(object):
                     ans[i] = j - i
                     break
         return ans
+    
+    # use a while loop within the for loop, it's still only O(n) time and space, because we only have to go through values once
+    class Solution(object):
+        def dailyTemperatures(self, temperatures):
+            """
+            :type temperatures: List[int]
+            :rtype: List[int]
+            """
+            stack = [0]
+            ans = [0 for i in temperatures]
+            
+            for i in range(1, len(temperatures)):  
+                while stack and temperatures[i] > temperatures[stack[-1]] :
+                    index = stack.pop()
+                    ans[index] = i - index
+                stack.append(i)
+            return ans
